2abb530878b7181f41921f5779a433233493fff8
armansamimi <armansamimi18@gmail.com>
2023-07-28 19:54:59 +0330

    Second commit

--- renameVideoFiles.py
+++ renameVideoFiles.py
@@ -1,11 +1,16 @@
+# This is a simple Script ro read Video files from a directory 
+# and the Change their names
+# based on what you want.
+
 import os
 
-# User-defined string to use for the file names
+# User-defined string to use for the file names : prefix
 my_string = "dep_client_02_"
 
 # Counter to keep track of the file names
 counter = 1
 
+# specify your path here
 input_dir = "E:\Arman\My Project\Project 48 - AiMental\Artificial Intelligence\DataSet\Therapy session - Video\Depression 02\Cropped_video"
 
 # Create output directory if it doesn't exist
@@ -16,6 +21,7 @@ if not os.path.exists("output"):
 for file_name in os.listdir(input_dir):
     if file_name.endswith(".mp4") or file_name.endswith(".avi") or file_name.endswith(".flv"):
         # Generate a new name for the file using the user-defined string and the counter
+        # user-defined string + Counter
         new_name = my_string + str(counter) + os.path.splitext(file_name)[1]
         
         # Read the video file
